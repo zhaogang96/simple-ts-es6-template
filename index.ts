8f09ccb0eebdc428ab81809770ceda42d6a4661c
zhaogang <wangyiemailz@126.com>
2020-03-23 15:36:03 +0800

    添加 简单项目模板

---
+++ index.ts
@@ -0,0 +1,12 @@
+// import "ts-polyfill/lib/index.js";
+// import "ts-polyfill/lib/es2016-array-include.js";
+import "core-js/es/object/assign";
+import "core-js/es/array/includes";
+
+const c = [1, 2, 42];
+console.log(c.includes(1));
+console.log(...c);
+async function test() {
+  console.log("2243f");
+}
+Object.assign({}, { a: { b: "122" } });
